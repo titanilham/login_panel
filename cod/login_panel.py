04fb9ea2ba9030b578ca61c0e44503e135f57a94
Ilham <86422270+titanilham@users.noreply.github.com>
2023-12-22 10:46:19 +0300

    Add files via upload

--- cod/login_panel.py
+++ cod/login_panel.py
@@ -1,1 +1,29 @@
-print("Hi")
+from getpass import getpass
+
+
+def login_panel(logins:list, passwords:list):
+    
+    # access status
+    entrance = False 
+    
+    # login writing 
+    login = input("Login: " )
+    
+    # password writing
+    password = getpass("Password: " )
+    
+    # login and password verification
+    try:
+        if login in logins:
+            if logins.index(login) == passwords.index(password):
+                # gaining access
+                entrance = True
+    except ValueError:
+        entrance = False
+    
+    
+    return entrance
+
+if __name__ == "__main__":
+    login_panel()
+    
